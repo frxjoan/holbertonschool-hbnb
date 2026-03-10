ad27bc7ae84edbb5fae5d75fe4c74bd7b95b7a60
frxjoan <12224@holbertonstudents.com>
2026-03-10 14:09:01 +0100

    feat(repository): add sqlalchemy repository to switch from in memory db to a sql db later

--- part3/app/persistence/repository.py
+++ part3/app/persistence/repository.py
@@ -5,6 +5,7 @@ implementation used by the service layer.
 """
 
 from abc import ABC, abstractmethod
+from app import db
 
 class Repository(ABC):
     """Define the base repository interface."""
@@ -138,3 +139,34 @@ class InMemoryRepository(Repository):
         """
         return next((obj for obj in self._storage.values()
                      if getattr(obj, attr_name) == attr_value), None)
+
+class SQLAlchemyRepository(Repository):
+    """Repository implementation using SQLAlchemy."""
+    def __init__(self, model):
+        self.model = model
+
+    def add(self, obj):
+        db.session.add(obj)
+        db.session.commit()
+
+    def get(self, obj_id):
+        return db.session.get(self.model, obj_id)
+
+    def get_all(self):
+        return self.model.query.all()
+
+    def update(self, obj_id, data):
+        obj = self.get(obj_id)
+        if obj:
+            for key, value in data.items():
+                setattr(obj, key, value)
+            db.session.commit()
+
+    def delete(self, obj_id):
+        obj = self.get(obj_id)
+        if obj:
+            db.session.delete(obj)
+            db.session.commit()
+
+    def get_by_attribute(self, attr_name, attr_value):
+        return self.model.query.filter(getattr(self.model, attr_name) == attr_value).first()
