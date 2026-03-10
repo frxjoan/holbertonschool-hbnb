acad071b8efe8a92a4cedf846fc8e9add63e4260
Alreix <alreixdev@gmail.com>
2026-03-10 11:57:04 +0100

    feat(init.py): adding the import for sqlalchemy and database initialisation

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -5,9 +5,11 @@ from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
+db = SQLAlchemy()
 
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
@@ -37,5 +39,6 @@ def create_app(config_class="config.DevelopmentConfig"):
 
     bcrypt.init_app(app)
     jwt.init_app(app)
+    db.init_app(app)
 
     return app
